4c6673451778eed45116747d259ce2784bbe2567
Rohit Kumar <115869119+rohitkumxr@users.noreply.github.com>
2024-09-06 13:45:18 +0000

    array basic

--- 02basic/01object.js
+++ 02basic/01object.js
@@ -5,6 +5,14 @@ const jsUser ={
 
 }
 
-console.log(jsUser.isLog);
+// console.log(jsUser.isLog);
 
-console.log(jsUser["isLog"]);
+// console.log(jsUser["isLog"]);
+
+let hero = ["a","b"];
+let zero = ["c","d"];
+
+const together = [...hero , ...zero]; // merge two array called spread operator
+console.log(together);
+
+// console.log(Array.of(hero,zero));
