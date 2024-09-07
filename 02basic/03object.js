638f604091442f26bfd5c9605ac9477b7792da3a
Rohit Kumar <115869119+rohitkumxr@users.noreply.github.com>
2024-09-07 10:32:43 +0000

    json

---
+++ 02basic/03object.js
@@ -0,0 +1,9 @@
+const subject ={
+    name : "rohit",
+    price : 999,
+    email : "rohit@gmail.com"
+}
+const {price : pz} = subject;
+// console.log(price);
+console.log(subject.price);
+console.log(pz);
